519906cdbb06a350aff6a906c7dd92f9659848a0
Diana Tsukanova <dtsukanova5@gmail.com>
2023-03-16 16:39:30 +0300

    update server.js

--- form/server.js
+++ form/server.js
@@ -5,6 +5,17 @@ const url = require('url');
 server = http.createServer(function (req, res) {
     res.setHeader("Content-Type", "application/json");
     const urlParams = url.parse(req.url);
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'OPTIONS, GET',
+        'Access-Control-Max-Age': 2592000
+    };
+
+    if (req.method === 'OPTIONS') {
+        res.writeHead(204, headers);
+        res.end();
+        return;
+    }
 
     if (urlParams.pathname == '/form') {
         res.writeHead(200, headers);
