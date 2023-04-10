513bcb70d3ac68c50dec2cdf2f7ba9ea79dee853
Diana Tsukanova <dtsukanova5@gmail.com>
2023-04-10 13:53:33 +0300

    feat: update2 my project

--- index.js
+++ index.js
@@ -6,10 +6,7 @@ window.addEventListener('load', function () {
 const button1 = document.getElementById('button1');
 const button2 = document.getElementById('button2');
 const header = document.getElementById('header');
-
-const leftBlock = document.querySelector('.left');
 const middleBlock = document.querySelector('.middle');
-const middleRow = document.getElementById('middle-row');
 
 button1.addEventListener('click', () => {
     header.classList.toggle('visible');
